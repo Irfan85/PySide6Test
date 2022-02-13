0f40c265ce9c1b844562fe6877076f71c57c137e
Irfan Muhit <irfan.muhit85@gmail.com>
2022-02-14 02:14:21 +0600

    2

--- Basics/simpleWindow.py
+++ Basics/simpleWindow.py
@@ -1,9 +1,24 @@
-from PySide6.QtWidgets import QApplication, QMainWindow
 import sys
+
+from PySide6.QtCore import QSize
+from PySide6.QtWidgets import QApplication, QMainWindow, QPushButton
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("Main Window")
+        self.setFixedSize(QSize(400, 300))
+
+        button = QPushButton("Click Me!")
+
+        self.setCentralWidget(button)
+
 
 app = QApplication(sys.argv)
 
-window = QMainWindow()
+window = MainWindow()
 window.show()
 
 app.exec()
